10d742425cd0f4090653aff36324d3c2b83c1741
ahmedbamhraz <ahmed.bamuhriz@gmail.com>
2024-07-30 15:34:34 +0300

    PSPPVG-10-Hacking Version 1

---
+++ Module_3/Program Hacking Version 1.py
@@ -0,0 +1,30 @@
+#GAME START
+print('DEBUG MODE')
+print('1 ATTEMPT(S) LEFT')
+print('')
+
+#PASSWORD
+print('PROVIDE')
+print('SETTING')
+print('CANTINA')
+print('CUTTING')
+print('HUNTERS')
+print('SURVIVE')
+print('HEARING')
+print('HUNTING')
+print('REALIZE')
+print('NOTHING')
+print('OVERLAP')
+print('FINDING')
+print('PUTTING')
+print('')
+
+#GUESS
+input('ENTER PASSWAORD >')
+
+#END THE GAME
+print('LOGCIN FALLURE - TERMINAL LOKED')
+print('')
+print('PLEASE CONTACT AN ADMINISTATOR')
+print('')
+print('PRESS ENTER TO EXIT')
