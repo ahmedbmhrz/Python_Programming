5b32ba59f6baffe88496393d3458c434fbfad247
ahmedbamhraz <ahmed.bamuhriz@gmail.com>
2024-08-06 00:00:52 +0300

    PSPPVG-18-quiz-Reflect on language concepts used in Hacking Version 2

---
+++ Module_4/quiz-Reflect on language concepts used in Hacking Version 2.py
@@ -0,0 +1,13 @@
+from uagame import Window
+from time import sleep
+window = Window('hello', 300, 200)
+guess = window.input_string('Enter string >', 0, 0)
+
+string_height = window.get_font_height()
+x_coordinate = window.get_width() - window.get_string_width(guess)
+y_coordinate = window.get_height() - string_height
+window.draw_string(guess, x_coordinate, y_coordinate)
+
+window.update()
+sleep(2)
+window.close()
